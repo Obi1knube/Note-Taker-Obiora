163cf483d04fa4436e3ab0bc2169e0995cfb8331
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2023-12-15 17:41:10 +0000

    npm i nodemon insstalled and added to the package. json folder

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-
 // Import necessary modules
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 // Set up the Express.js app
 const app = express();
@@ -11,24 +10,14 @@ const PORT = process.env.PORT || 3001;
 // Middleware to handle JSON data
 app.use(express.json());
 // Middleware to serve static files
-app.use(express.static('public'));
-
-// Route to return the notes.html file
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-});
-
-// Route to return the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.use(express.static("public"));
 
 // Route to read the db.json file and return all saved notes as JSON
-app.get('/api/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
     if (err) {
       console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
+      res.status(500).json({ error: "Internal server error" });
     } else {
       const notes = JSON.parse(data);
       res.json(notes);
@@ -37,26 +26,31 @@ app.get('/api/notes', (req, res) => {
 });
 
 // Route to receive a new note, add it to the db.json file, and return the new note to the client
-app.post('/api/notes', (req, res) => {
+app.post("/api/notes", (req, res) => {
   const newNote = req.body;
 
-  fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
     if (err) {
       console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
+      res.status(500).json({ error: "Internal server error" });
     } else {
       const notes = JSON.parse(data);
       const newNoteWithId = { ...newNote, id: generateUniqueId() };
       notes.push(newNoteWithId);
 
-      fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), 'utf8', (err) => {
-        if (err) {
-          console.error(err);
-          res.status(500).json({ error: 'Internal server error' });
-        } else {
-          res.json(newNoteWithId);
+      fs.writeFile(
+        path.join(__dirname, "db", "db.json"),
+        JSON.stringify(notes),
+        "utf8",
+        (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: "Internal server error" });
+          } else {
+            res.json(newNoteWithId);
+          }
         }
-      });
+      );
     }
   });
 });
@@ -66,9 +60,19 @@ function generateUniqueId() {
   return Math.random().toString(36).substr(2, 9);
 }
 
+// Route to return the notes.html file
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// Route to return the index.html file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
 
-module.exports = app;
+//module.exports = app;
--- server.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('./server.js');
-
-
-test('GET /api/notes should return all saved notes as JSON', async () => {
-    const response = await request(app).get('/api/notes');
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual(/* expected response body */);
-  });
